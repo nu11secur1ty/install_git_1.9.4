b8dbc3c6e0c3264e071f179614bea089f4fddcfe
nu11secur1ty <systadmi@abv.bg>
2016-03-08 22:21:27 +0200

    Update install_git_1.9.4.sh

--- install_git_1.9.4.sh
+++ install_git_1.9.4.sh
@@ -10,15 +10,14 @@
  wget https://www.kernel.org/pub/software/scm/git/git-1.9.4.tar.gz
       tar xzf git-1.9.4.tar.gz
  # go to git dir and install it
-	  cd git-1.9.4
+	  cd /usr/src/git-1.9.4
   make prefix=/usr/local/git all
   make prefix=/usr/local/git install
   echo "export PATH=$PATH:/usr/local/git/bin" >> /etc/bashrc
   source /etc/bashrc
   # cleaning
-              cd /usr/src/
-              rm -rf git-1.9.4
-              rm -rf git-1.9.4.tar.gz
+              rm -rf /usr/src/git-1.9.4
+              rm -rf /usr/src/git-1.9.4.tar.gz
        # git version
 	      git --version
 		  sleep 1
